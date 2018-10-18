63f5c8e5019f9956dc9ff1bedcb5ea838b64e983
Raul Gomez <byraul0209@gmail.com>
2018-10-18 08:32:19 -0600

    Funcionalidad del tablero

--- js/app.js
+++ js/app.js
@@ -1,5 +1,11 @@
+document.addEventListener("keydown", movimiento);
 var canvas = document.getElementById('fondo'); //Busca el archivo y lo guarda en la variable
 var lapiz = canvas.getContext('2d');
+const DIMENSION = 80;
+var x = random(0, 5) * 80;
+var y = random(0, 5) * 80;
+var matriz = new Array(6);
+var porcina;
 
 var fondo = {
     url: './imagenes/tile.png',
@@ -25,6 +31,8 @@ var cuchillo = {
     cargaOK: false
 };
 
+
+
 cuchillo.imagen = new Image();
 cuchillo.imagen.src = cuchillo.url;
 
@@ -44,12 +52,12 @@ fondo.imagen.addEventListener("load", function(){
 
 vaca.imagen.addEventListener("load",function(){
     vaca.cargaOK = true;
-    dibujar();
+    // dibujar();
 });
 
 cerdo.imagen.addEventListener("load",function(){
     cerdo.cargaOK = true;
-   dibujar();
+//    dibujar();
 });
 
 cuchillo.imagen.addEventListener("load",function(){
@@ -57,23 +65,123 @@ cuchillo.imagen.addEventListener("load",function(){
     dibujar();
 });
 
+iniciarMatriz();
+inicializarVacas();
+inicializarCerdos();
+dibujar();
+
 function dibujar(){
     if(fondo.cargaOK == true){
         lapiz.drawImage(fondo.imagen,0,0);
     }
 
-    if(vaca.cargaOK == true){
-        lapiz.drawImage(vaca.imagen,150,100);
+    dibujarMatriz();
+   if(cuchillo.cargaOK == true){
+       lapiz.drawImage(cuchillo.imagen,x,y);
+   }
+}
+
+   var tecla={
+       LEFT: 37,
+       UP: 38,
+       RIGHT: 39,
+       DOWN: 40,
+       ENTER: 13
+   }
+
+   function movimiento(evento){
+       switch(evento.keyCode){
+        case tecla.LEFT:
+        if (x > 0) {
+            x = x - DIMENSION;
+            dibujar();
+        }
+         break;
+
+        case tecla.UP:
+        if (y > 0){
+            y = y - DIMENSION;
+            dibujar();
+        }
+        break;
+
+        case tecla.DOWN:
+        if(y < 400){
+            y = y + DIMENSION;
+            dibujar();
+        }
+       break;
+        
+        case tecla.RIGHT:
+        if (x < 400){
+            x = x + DIMENSION;
+            dibujar();
+        }
+        break;
+
+        case tecla.ENTER:
+        alert(random(0,5));
+        break;
+       }
     }
 
-   if(cerdo.cargaOK == true){
-    lapiz.drawImage(cerdo.imagen,300,200);
+   function random(minimo,maximo){
+return Math.floor(Math.random() * (maximo-minimo +1)) + minimo;
    }
 
-   if(cuchillo.cargaOK == true){
-       lapiz.drawImage(cuchillo.imagen,150,150);
-   }
+ 
+function iniciarMatriz() {
+      for (var i = 0; i < matriz.length; i++) {
+        matriz[i] = new Array(6);
+        for (var j = 0; j < matriz.length; j++) {
+            matriz[i][j] = 'x';
+        }
+    }
+}
+
+function inicializarVacas() {
+    var numero = random(0, 5);
+    for (var i = 0; i <= numero; i++) {
+        var linea = random(0, 5);
+        var columna = random(0, 5);
+        if (matriz[linea][columna] == 'x') {
+            matriz[linea][columna] = 'v';
+        }
+    }
+}
+
+function inicializarCerdos() {
+    var numero = random(0, 5);
+    for (var i = 0; i <= numero; i++) {
+        var linea = 0 + random(0, 5);
+        var columna = 0 + random(0, 5);
+        if (matriz[linea][columna] == 'x') {
+            matriz[linea][columna] = 'c';
+        }
+        
+    }
     
 }
 
+function dibujarMatriz() {
+    for (var i = 0; i < matriz.length; i++) {
+        for (var j = 0; j < matriz.length; j++) {
+            if (matriz[i][j] == 'v') {
+                lapiz.drawImage(vaca.imagen, i * DIMENSION, j * DIMENSION);
+            } else if (matriz[i][j] == 'c') {
+                lapiz.drawImage(cerdo.imagen, i * DIMENSION, j * DIMENSION);
+            }
+        }
+    }
+}
 
+// function generarCerdosMalos(){
+//  for(var i = 0; i < numero; i++) {
+//     var linea = lineaCerdo
+//     var columna = columnaCerdo
+//     if (matriz[linea][columna] == 'c') {
+//         matriz[linea][columna] = 'p'
+//     }
+//   }
+// }
+// alert(lineaCerdo)
